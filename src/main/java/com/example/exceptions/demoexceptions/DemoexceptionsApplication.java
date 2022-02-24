ded022529649c63d4ffc9503021b38231e054fa2
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-02-24 18:29:43 +0200

    first

---
+++ src/main/java/com/example/exceptions/demoexceptions/DemoexceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptions.demoexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoexceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoexceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exceptions/demoexceptions/DemoexceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptions.demoexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoexceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
